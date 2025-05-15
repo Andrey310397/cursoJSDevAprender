8d4d8b25d00619901faee240d0ad21d523c05fa4
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-15 17:36:22 -0300

    Constantes

--- aula.js
+++ aula.js
@@ -1,9 +1,3 @@
-// Crie um algoritmo que imprima 3 variáveis diferentes.
+const valorIngressoAdulto = 20;
 
-let number = 1;
-let name = 'Carlos'
-let price = 34.90
-
-console.log(number)
-console.log(name)
-console.log(price)
+console.log(valorIngressoAdulto);
