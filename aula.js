f592820adfe090c6bf9e51cf345a830e93bf8578
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-16 15:32:43 -0300

    Objetos

--- aula.js
+++ aula.js
@@ -1,5 +1,18 @@
-let nome = 'Rafael'; //String literal
-let idade = 25; // number literal
-let estaAprovado = true; //boolean
-let sobrenome = undefined; // Undefined
-let corSelecionado = null // Redefinir um valor
+// Objetos -> Irá juntar informações em um lugar só que tem como acessar pela propriedade daquele objeto
+
+let nome = 'Rafael';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
+
+//Objeto
+let pessoa = {
+    //Key  Value
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de souza'
+};
+
+console.log(pessoa);
