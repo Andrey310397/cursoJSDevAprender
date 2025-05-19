fc493645d89842b0d836acc27a9e465e67fcd12a
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-19 19:06:28 -0300

    for..in loop

--- aula.js
+++ aula.js
@@ -23,8 +23,26 @@
 
 // Do..While loop
 
-let i = 0;
-do {
-    console.log('digitando', i);
-    i++;
-} while (i <= 10)
+// let i = 0;
+// do {
+//     console.log('digitando', i);
+//     i++;
+// } while (i <= 10)
+
+// ----------------------------------------------------
+
+// for..in loop
+const pessoa = {
+    nome: 'Andrey',
+    idade: 25
+};
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa.nome);
+}
+
+const cores = ['vermelho', 'azul', 'verde'];
+
+for (let indice in cores) {
+    console.log(indice, cores[indice]);
+}
