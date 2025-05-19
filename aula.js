19c3d47977fd6844034708b0584dda94503828fe
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-19 18:55:38 -0300

    Loop Do..While

--- aula.js
+++ aula.js
@@ -10,11 +10,21 @@
 
 // While loop
 
-let i = 5;
+// let i = 5;
 
-while (i >= 1) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    }
-    i--;
-}
+// while (i >= 1) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+//     i--;
+// }
+
+// ----------------------------------------------------
+
+// Do..While loop
+
+let i = 0;
+do {
+    console.log('digitando', i);
+    i++;
+} while (i <= 10)
