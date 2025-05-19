d456286c8aa44602d52709c66b4e26fbf27638c6
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-19 18:51:49 -0300

    Loop while

--- aula.js
+++ aula.js
@@ -1,7 +1,20 @@
 // Laço for
 
-for (let i = 0; i <= 5; i++) {
+// for (let i = 0; i <= 5; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// ----------------------------------------------------
+
+// While loop
+
+let i = 5;
+
+while (i >= 1) {
     if (i % 2 !== 0) {
         console.log(i);
     }
+    i--;
 }
