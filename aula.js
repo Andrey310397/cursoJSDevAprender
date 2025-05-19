4ae474b7763b91b06c7b540ce6f217c16dd4c5d4
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-19 17:57:37 -0300

    Switch case

--- aula.js
+++ aula.js
@@ -1,11 +1,17 @@
-// Condicionais - If .. Else
+// Switch case
+let permissao = 'diretor'; // comum, gerente, diretor
 
-let hora = 13
+switch (permissao) {
+    case 'comum':
+        console.log('usuário comum');
+        break;
+    case 'gerente':
+        console.log('usuário gerente');
+        break;
+    case 'diretor':
+        console.log('usuário diretor');
+        break;
 
-if (hora > 6 && hora < 12) {
-    console.log('Bom dia');
-} else if (hora > 12 && hora < 18) {
-    console.log('Boa tarde');
-} else {
-    console.log('Boa noite');
+    default:
+        console.log('Usuário não reconhecido!');
 }
