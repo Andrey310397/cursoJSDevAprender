1de2a6bd9f8d0a5778264a7b71e5af5f872e1202
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-20 20:59:27 -0300

    Mini projeto - Múltiplos de 3 e 5

--- aula.js
+++ aula.js
@@ -1,19 +1,22 @@
-// Mini projeto - Encontre o String
-// exibir somente as propriedades do tipo string que estão nesse objeto
+// Mini projeto - Multiplos de 3 e 5
+// soma de todos os múltiplos de 3 e 5
+// Armazenar os múltiplos de 3
+// Armazenar os múltiplos de 5
+// Somar os resultados
 
-const filme = {
-    titulo: 'Vingadores',
-    ano: 2008,
-    diretor: 'Robin',
-    personagem: 'Thor'
-}
+somar(10);
 
-exibirPropriedades(filme);
+function somar(limite) {
+    let multiplos3 = 0;
+    let multiplos5 = 0;
 
-function exibirPropriedades(obj) {
-    for (let chave in obj) {
-        if (typeof obj[chave] === 'string') {
-            console.log(chave, obj[chave]);
+    for (i = 0; i <= limite; i++) {
+        if (i % 3 === 0) {
+            multiplos3 += i;
+        }
+        if (i % 5 === 0) {
+            multiplos5 += i;
         }
     }
+    console.log(multiplos3 + multiplos5);
 }
