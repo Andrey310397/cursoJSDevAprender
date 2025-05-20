5831302d3b715874d9f886817d031c67ec45c90b
Andrey Oliveira <andreyoliveira222@gmail.com>
2025-05-20 20:17:01 -0300

    Mini projeto - Par ou impar

--- aula.js
+++ aula.js
@@ -1,30 +1,14 @@
-// Mini projeto - Medidor de velocidade
-// Velocidade máxima de 70km - Ok
-// A cada 5km ganha 1 ponto na carteira
-// Precisará usar a função Math.Floor()
-// Caso pontos maior que 12 -> Carteira Suspensa
-verificarVelocidade(80);
+// Mini projeto - Par ou impar
+// Função exibe se cada valor é par ou impar
 
-function verificarVelocidade(velocidade) {
-    const velocidadeMaxima = 70;
-    const kmPorPonto = 5;
+exibirTipo(5);
 
-    if (velocidade <= velocidadeMaxima) {
-        console.log('Velocidade: ' + velocidade + ' km/h');
-        console.log('Status: Ok');
-
-        console.log('Pontos: 0');
-    } else {
-        const excessoVelocidade = velocidade - velocidadeMaxima;
-
-        let pontos = Math.floor(excessoVelocidade / kmPorPonto);
-
-        console.log('Velocidade: ' + velocidade + ' km/h');
-        console.log('Status: Acima da velocidade');
-        console.log('Pontos: ' + pontos);
-
-        if (pontos >= 12) {
-            console.log('Carteira Suspensa');
+function exibirTipo(limite) {
+    for (let i = 0; i <= limite; i++) {
+        if (i % 2 === 0) {
+            console.log(i, 'Par');
+        } else {
+            console.log(i, 'Impar');
         }
     }
 }
